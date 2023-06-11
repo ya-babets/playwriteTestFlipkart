186570de944c0aa400cf2ce84c81ffd565df4f40
ya-babets <bebtsya@gmail.com>
2023-06-11 13:08:59 +0100

    updateYamlfile

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,7 +46,7 @@ const playwrightTestConfig: PlaywrightTestConfig = {
         ignoreHTTPSErrors: true,
         /* Base URL to use in actions like `await page.goto('/')`. */
         // baseURL: 'http://localhost:3000',
-        headless: false,
+        headless: true,
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
